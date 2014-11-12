06e87bee9df7a90bce07d26ce3c6c44250c4fe48
David Robles <master-nevi@users.noreply.github.com>
2014-11-12 08:00:16 -0800

    Create blank sample project.

---
+++ sample-projects/ControlPointDemo/ControlPointDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ControlPointDemo
+//
+//  Created by David Robles on 11/12/14.
+//  Copyright (c) 2014 David Robles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sample-projects/ControlPointDemo/ControlPointDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ControlPointDemo
+//
+//  Created by David Robles on 11/12/14.
+//  Copyright (c) 2014 David Robles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
