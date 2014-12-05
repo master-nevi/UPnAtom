0873f1175bcee7a5a5ae89c480952dadee2eaf83
David Robles <master-nevi@users.noreply.github.com>
2014-12-04 18:05:20 -0800

    Fix copyright.

--- upnpx-swift/AbstractXMLParser.swift
+++ upnpx-swift/AbstractXMLParser.swift
@@ -3,7 +3,7 @@
 //  upnpx
 //
 //  Created by David Robles on 11/12/14.
-//  Copyright (c) 2014 Bruno Keymolen. All rights reserved.
+//  Copyright (c) 2014 David Robles. All rights reserved.
 //
 
 import Foundation
