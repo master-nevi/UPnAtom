509c3f6147b054a26c67f5ae3a87a4e935b38922
David Robles <master-nevi@users.noreply.github.com>
2015-03-19 14:03:24 -0700

    Replace old style framework imports with "@import" syntax.

--- Examples/ControlPointDemo_ObjC/ControlPointDemo/main.m
+++ Examples/ControlPointDemo_ObjC/ControlPointDemo/main.m
@@ -21,7 +21,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
