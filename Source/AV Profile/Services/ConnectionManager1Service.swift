9809f927df5b7bed75f3b6fd754b3c18dbb8e762
David Robles <master-nevi@users.noreply.github.com>
2015-04-27 16:12:47 -0700

    Implement ConnectionManager1Service actions: "GetProtocolInfo", "PrepareForConnection", "ConnectionComplete", "GetCurrentConnectionsIDs", "GetCurrentConnectionInfo"

--- Source/AV Profile/Services/ConnectionManager1Service.swift
+++ Source/AV Profile/Services/ConnectionManager1Service.swift
@@ -24,7 +24,85 @@
 import Foundation
 
 public class ConnectionManager1Service: AbstractUPnPService {
+    public func getProtocolInfo(success: (source: [String], sink: [String]) -> Void, failure: (error: NSError) -> Void) {
+        let parameters = SOAPRequestSerializer.Parameters(soapAction: "GetProtocolInfo", serviceURN: urn, arguments: nil)
+        
+        soapSessionManager.POST(self.controlURL.absoluteString!, parameters: parameters, success: { (task: NSURLSessionDataTask!, responseObject: AnyObject!) -> Void in
+            let responseObject = responseObject as? [String: String]
+            success(source: responseObject?["Source"]?.componentsSeparatedByString(",") ?? [String](), sink: responseObject?["Sink"]?.componentsSeparatedByString(",") ?? [String]())
+            }, failure: { (task: NSURLSessionDataTask!, error: NSError!) -> Void in
+                failure(error: error)
+        })
+    }
     
+    public func prepareForConnection(#remoteProtocolInfo: String, peerConnectionManager: String, peerConnectionID: String, direction: String, success: (connectionID: String?, avTransportID: String?, renderingControlServiceID: String?) -> Void, failure:(error: NSError) -> Void) {
+        let arguments = [
+            "RemoteProtocolInfo" : remoteProtocolInfo,
+            "PeerConnectionManager" : peerConnectionManager,
+            "PeerConnectionID" : peerConnectionID,
+            "Direction" : direction]
+        
+        let parameters = SOAPRequestSerializer.Parameters(soapAction: "PrepareForConnection", serviceURN: urn, arguments: arguments)
+        
+        // Check if the optional SOAP action "PrepareForConnection" is supported
+        supportsSOAPAction(actionParameters: parameters) { (isSupported) -> Void in
+            if isSupported {
+                self.soapSessionManager.POST(self.controlURL.absoluteString!, parameters: parameters, success: { (task: NSURLSessionDataTask!, responseObject: AnyObject!) -> Void in
+                    let responseObject = responseObject as? [String: String]
+                    success(connectionID: responseObject?["ConnectionID"], avTransportID: responseObject?["AVTransportID"], renderingControlServiceID: responseObject?["RcsID"])
+                    }, failure: { (task: NSURLSessionDataTask!, error: NSError!) -> Void in
+                        failure(error: error)
+                })
+            }
+            else {
+                failure(error: createError("SOAP action '\(parameters.soapAction)' unsupported by service \(self.urn) on device \(self.device?.friendlyName)"))
+            }
+        }
+    }
+    
+    public func connectionComplete(#connectionID: String, success: () -> Void, failure:(error: NSError) -> Void) {
+        let arguments = ["ConnectionID" : connectionID]
+        
+        let parameters = SOAPRequestSerializer.Parameters(soapAction: "ConnectionComplete", serviceURN: urn, arguments: arguments)
+        
+        // Check if the optional SOAP action "ConnectionComplete" is supported
+        supportsSOAPAction(actionParameters: parameters) { (isSupported) -> Void in
+            if isSupported {
+                self.soapSessionManager.POST(self.controlURL.absoluteString!, parameters: parameters, success: { (task: NSURLSessionDataTask!, responseObject: AnyObject!) -> Void in
+                    success()
+                    }, failure: { (task: NSURLSessionDataTask!, error: NSError!) -> Void in
+                        failure(error: error)
+                })
+            }
+            else {
+                failure(error: createError("SOAP action '\(parameters.soapAction)' unsupported by service \(self.urn) on device \(self.device?.friendlyName)"))
+            }
+        }
+    }
+    
+    public func getCurrentConnectionIDs(success: (connectionIDs: [String]) -> Void, failure: (error: NSError) -> Void) {
+        let parameters = SOAPRequestSerializer.Parameters(soapAction: "GetCurrentConnectionIDs", serviceURN: urn, arguments: nil)
+        
+        soapSessionManager.POST(self.controlURL.absoluteString!, parameters: parameters, success: { (task: NSURLSessionDataTask!, responseObject: AnyObject!) -> Void in
+            let responseObject = responseObject as? [String: String]
+            success(connectionIDs: responseObject?["ConnectionIDs"]?.componentsSeparatedByString(",") ?? [String]())
+            }, failure: { (task: NSURLSessionDataTask!, error: NSError!) -> Void in
+                failure(error: error)
+        })
+    }
+    
+    public func getCurrentConnectionInfo(#connectionID: String, success: (renderingControlServiceID: String?, avTransportID: String?, protocolInfo: String?, peerConnectionManager: String?, peerConnectionID: String?, direction: String?, status: String?) -> Void, failure: (error: NSError) -> Void) {
+        let arguments = ["ConnectionID" : connectionID]
+        
+        let parameters = SOAPRequestSerializer.Parameters(soapAction: "GetCurrentConnectionInfo", serviceURN: urn, arguments: arguments)
+        
+        soapSessionManager.POST(self.controlURL.absoluteString!, parameters: parameters, success: { (task: NSURLSessionDataTask!, responseObject: AnyObject!) -> Void in
+            let responseObject = responseObject as? [String: String]
+            success(renderingControlServiceID: responseObject?["RcsID"], avTransportID: responseObject?["AVTransportID"], protocolInfo: responseObject?["ProtocolInfo"], peerConnectionManager: responseObject?["PeerConnectionManager"], peerConnectionID: responseObject?["PeerConnectionID"], direction: responseObject?["Direction"], status: responseObject?["Status"])
+            }, failure: { (task: NSURLSessionDataTask!, error: NSError!) -> Void in
+                failure(error: error)
+        })
+    }
 }
 
 /// for objective-c type checking
