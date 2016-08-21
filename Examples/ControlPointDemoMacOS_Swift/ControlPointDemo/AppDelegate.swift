90005fb15a39c2a3c1f50abe9b83cf37a396c50b
David Robles <master-nevi@users.noreply.github.com>
2016-08-21 14:22:40 -0700

    Add MacOS demo.

---
+++ Examples/ControlPointDemoMacOS_Swift/ControlPointDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ControlPointDemo
+//
+//  Created by David Robles on 8/21/16.
+//  Copyright © 2016 David Robles. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
